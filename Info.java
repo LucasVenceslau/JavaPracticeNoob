b7079bdaa76afc0f60ee039c0c5afef395bc1cb4
LucasVenceslau <72529025+LucasVenceslau@users.noreply.github.com>
2021-10-20 11:51:26 -0300

    OlaMundo.java, Info.java
    Files with the first steps in Java. In these files, I have practiced basic java conventions, compilation using cmd (javac) and testing using cmd (java).

---
+++ Info.java
@@ -0,0 +1,10 @@
+//Exercício para imprimir informações pessoais na tela
+
+public class Info {
+	public static void main(String[] args){
+		System.out.println("Nome: Lucas");
+		System.out.println("Endereço: Ribeirão bão"); //os acentos saem desconfigurados no cmd
+		System.out.println("Telefone: 99999999");
+		System.out.println("E-mail: lucas_venceslau@yahoo.com.br");
+}
+}
---
+++ OlaMundo.java
@@ -0,0 +1,7 @@
+//Programa Ola Mundo
+
+public class OlaMundo {
+	public static void main(String[] args){
+		System.out.print("Ola mundo!");
+						}
+		      }
