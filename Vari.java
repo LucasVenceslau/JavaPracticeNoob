e42d66a4af24c44138d04e437e05d7a0216dbe41
LucasVenceslau <72529025+LucasVenceslau@users.noreply.github.com>
2021-10-28 16:55:20 -0300

    Exercício de declaração de variáveis

---
+++ Vari.java
@@ -0,0 +1,16 @@
+//Exercício 1 sobre declaração de variáveis
+
+public class Vari {			//sem acento porque o compilador não aceita
+	public static void main(String[] args){
+
+//Declaração de variáveis:
+
+	int contador = 1;
+	char a = 3;
+	double decimal = 0.5;
+	boolean vouf = true;
+
+//Exibição das variáveis no cmd:
+	System.out.println("Minhas variaveis sao: "+contador+", "+a+", "+decimal+", "+vouf+"."); //sem acento porque tais caracteres não são exibidos no cmd.
+						}
+		}
