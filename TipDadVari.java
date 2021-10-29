60b51dfc981d601d9e0cd5ced152a8a3932fe814
LucasVenceslau <72529025+LucasVenceslau@users.noreply.github.com>
2021-10-29 14:55:21 -0300

    Declaração de variáveis com typecast

---
+++ TipDadVari.java
@@ -0,0 +1,18 @@
+//Exercício de declaração de dados e atribuições
+
+public class TipDadVari{
+	public static void main(String[] args){
+		System.out.println("Hello world, estou aprendendo formas diferentes de atribuicao de variaveis");
+//Atribuição usando typecast
+	int idade = (int) 3;	
+	float salario = (float) 1500.50;
+	char inicial = (char) 'L';
+	boolean casado = (boolean) true;
+
+		System.out.println("idade: "+idade);
+		System.out.println("salario: "+salario);
+		System.out.println("inicial: "+inicial);
+		System.out.println("Casado: "+casado);
+
+	}
+}
