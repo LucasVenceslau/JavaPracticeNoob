1e9ee753e2ad7caa883cdbc368ce087ffb02aa44
LucasVenceslau <72529025+LucasVenceslau@users.noreply.github.com>
2021-11-04 13:51:05 -0300

    Exercício de média com operadores aritméticos

---
+++ Med.java
@@ -0,0 +1,11 @@
+//Exercício de média com operadores aritméticos
+
+public class Med{
+	public static void main(String[] args){
+	double nota1 = 5, nota2 = 3.5, nota3 = 9.5, media;
+	media = (nota1+nota2+nota3)/3;
+	System.out.println("Sua media final e "+media);//sem acento para rodar no cmd
+	
+	
+	}
+}
