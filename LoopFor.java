a1f299f7688f880929d87bec2f49dc30a5f9cfc1
LucasVenceslau <72529025+LucasVenceslau@users.noreply.github.com>
2021-11-12 15:08:15 -0300

    Exercício básico de loop com for
    Exibir na tela os números de 1 a 100.

---
+++ LoopFor.java
@@ -0,0 +1,11 @@
+//Exercício de loop com for para exibir na tela do cmd os números de 1 a 100. Sem acento para rodar no cmd. Feito no Bloco de Notas.
+
+public class LoopFor {
+
+	public static void main (String[] args) {
+	
+		for (int i = 1;i <= 100; i++){ //o contador i pode ser iniciado fora do for, se fizer isso, não precisa declarar o tipo de novo.
+		System.out.println(i + "\n");
+		}
+	}
+}
